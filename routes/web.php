b208ff65454143119c9942be98648366aa287938
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2023-04-06 12:04:40 -0300

    feat(): cria as rotas

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome')
-    ->name('home');
-Route::view('/test', 'test')
-    ->name('test');
+Route::view('/exercicios', 'exercicios')
+    ->name('exercicios');
+
+Route::group(['prefix' => 'aula'], function () {
+    Route::view('/01', 'aula.01')
+        ->name('aula01');
+});
